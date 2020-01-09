864a08c6c70e5543400cf2593b12941c380fe52b
ppasq <paolo.pasquali@gmail.com>
2020-01-09 17:54:49 +0100

    Use output.json

--- search/views.py
+++ search/views.py
@@ -6,10 +6,10 @@ from django.template import loader
 import json
 
 def index(request):
-    f = open('pippo.json')
+    f = open('output.json')
     json_string = f.read()
     f.close()
     data = json.loads(json_string)
     #return HttpResponse("Ciao. Questa è la risposta.")
     #return HttpResponse(data)
-    return render(request, 'search/index.html', data)
+    return render(request, 'search/index.html', {'data': data})
