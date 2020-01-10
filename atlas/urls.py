f84b3bd363c67c8d49fee2136f19ad1bf2b27f81
ppasq <paolo.pasquali@gmail.com>
2020-01-10 10:29:13 +0100

    Use static files

--- atlas/urls.py
+++ atlas/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='search/', permanent=True)),
     path('search/', include('search.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
